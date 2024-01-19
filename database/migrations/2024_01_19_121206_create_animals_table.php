d9a90bdf38f620c2833b09f4b36bc18eeff71fd7
D0nP3ngu1n <efrengcantero@gmail.com>
2024-01-19 13:15:58 +0100

    hoja 7

---
+++ database/migrations/2024_01_19_121206_create_animals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->eespecie();
+            $table->peso();
+            $table->altura();
+            $table->fechaNacimiento();
+            $table->imagen();
+            $table->alimentacion();
+            $table->descripcion();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
